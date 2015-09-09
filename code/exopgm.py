3a43c8f72ea3f2b20e154dd749d25f39e6cf7d82
davidwhogg <david.hogg@nyu.edu>
2015-09-09 15:07:56 -0400

    first draft of figures

--- code/exopgm.py
+++ code/exopgm.py
@@ -4,7 +4,9 @@ rc("text", usetex=True)
 
 import daft
 
-pgm = daft.PGM([3.6, 2.7], origin=[1.15, 0.65])
+foo = [4., 3.]
+origin = [1., 0.4]
+pgm = daft.PGM(foo, origin=origin)
 pgm.add_node(daft.Node("exoplanet", r"exoplanet", 1.8, 2, aspect=2.0))
 pgm.add_node(daft.Node("star", r"star", 3, 2, aspect=2.0))
 pgm.add_node(daft.Node("camera", r"camera", 4.2, 2, aspect=2.0))
@@ -16,3 +18,20 @@ pgm.render()
 pgm.figure.savefig("threecauses.pdf")
 pgm.figure.savefig("threecauses.png", dpi=150)
 
+pgm.add_node(daft.Node("exopop", r"exopop", 1.8, 3, aspect=2.0))
+pgm.add_node(daft.Node("starpop", r"starpop", 3, 3, aspect=2.0))
+pgm.add_edge("exopop", "exoplanet")
+pgm.add_edge("starpop", "star")
+pgm.add_edge("star", "exoplanet")
+pgm.add_plate(daft.Plate([1.2, 0.5, 3.6, 2.], label=r"light curves"))
+pgm.render()
+pgm.figure.savefig("morecauses.pdf")
+pgm.figure.savefig("morecauses.png", dpi=150)
+
+pgm2 = daft.PGM(foo, origin=origin)
+pgm2.add_node(daft.Node("kittens", r"kittens?", 3, 2, aspect=2.0))
+pgm2.add_node(daft.Node("data", r"data", 3, 1, aspect=2.0, observed=True))
+pgm2.add_edge("kittens", "data")
+pgm2.render()
+pgm2.figure.savefig("onecause.pdf")
+pgm2.figure.savefig("onecause.png", dpi=150)
